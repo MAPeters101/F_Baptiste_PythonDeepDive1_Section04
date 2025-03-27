5afcb6748dc566ba91d206ca9fecd0619f8a3d4a
Mark Peters <mapeters101@earthlink.net>
2025-03-27 15:41:29 -0400

    Work on lesson033 from 5:30 to 9:25.

--- lesson033.py
+++ lesson033.py
@@ -21,9 +21,32 @@ print(int("A", 11))
 #print(int("B", 11))
 print('-'*80)
 
+print(bin(10))
+print(bin(5))
+print(oct(10))
+print(hex(255))
 
+a = int('101', 2)
+print(a)
+b = 0b101
+print(b)
+print('-'*80)
 
-
+def from_base10(n, b):
+    if b < 2:
+        raise ValueError('Base b must be >= 2')
+    if n < 0:
+        raise ValueError('Number n must be >= 0')
+    if n == 0:
+        return [0]
+    digits = []
+    while n > 0:
+        # m, n = n%b, n//b
+        # m = n % b
+        # n = n // b
+        n, m = divmod(n, b)
+        digits.insert(0, m)
+    return digits
 
 
 
