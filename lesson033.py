e4603f2644382b24ca898212a7b5c21ef482bccb
Mark Peters <mapeters101@earthlink.net>
2025-03-27 15:08:44 -0400

    Work on lesson033 to 2:15.

---
+++ lesson033.py
@@ -0,0 +1,16 @@
+print(type(10))
+help(int)
+a = int()
+print(a)
+print(int(10.5))
+print(int(10.99999))
+print(int(True))
+print(int(False))
+import fractions
+a = fractions.Fraction(22, 7)
+print(a)
+print(float(a))
+print(int(a))
+print('-'*80)
+
+
