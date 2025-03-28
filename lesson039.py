7d7a5cff9b6e864cc7b3addcc581b9cc484bf61d
Mark Peters <mapeters101@earthlink.net>
2025-03-28 01:25:21 -0400

    Work on lesson039 to 2:25.

---
+++ lesson039.py
@@ -0,0 +1,21 @@
+x = 0.1
+print(format(x, '.25f'))
+print(x)
+print()
+
+x = 0.125
+print(format(x, '.25f'))
+print(x)
+print()
+
+x = 0.125 + 0.125 + 0.125
+y = 0.375
+print(x == y)
+print()
+
+x = 0.1 + 0.1 + 0.1
+y = 0.3
+print(x == y)
+print('-'*80)
+
+
