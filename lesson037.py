be298ad821a257c503181b0c490976644d5a6ec1
Mark Peters <mapeters101@earthlink.net>
2025-03-28 01:12:50 -0400

    Work on lesson037 to 2:00.

---
+++ lesson037.py
@@ -0,0 +1,13 @@
+help(float)
+
+print(float(10))
+print(float(10.4))
+print(float('12.5'))
+#print(float('22/7'))
+
+from fractions import Fraction
+a = Fraction('22/7')
+print(float(a))
+print('-'*80)
+
+
