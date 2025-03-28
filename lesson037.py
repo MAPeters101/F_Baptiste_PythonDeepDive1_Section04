2d3bdcab761a7c1bac9254fb2fe88f263ae8caed
Mark Peters <mapeters101@earthlink.net>
2025-03-28 01:18:01 -0400

    Work on lesson037 from 2:00 to 3:50.

--- lesson037.py
+++ lesson037.py
@@ -10,4 +10,18 @@ a = Fraction('22/7')
 print(float(a))
 print('-'*80)
 
+print(float(0.1))
+print(format(0.1, '.15f'))
+print(format(0.1, '.25f'))
+
+print(0.125)
+print(1/8)
+
+print(format(0.125, '.25f'))
+print('-'*80)
+
+
+
+
+
 
