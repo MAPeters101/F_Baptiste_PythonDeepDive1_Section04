208a578a10cc4263db8567da13dffb6007c06281
Mark Peters <mapeters101@earthlink.net>
2025-03-27 23:47:38 -0400

    Complete work on lesson035 from 11:00.

--- lesson035.py
+++ lesson035.py
@@ -36,8 +36,34 @@ print(float(x))
 y = Fraction(math.sqrt(2))
 print(y)
 print(float(y))
+print('-'*80)
 
+a = 0.125
+print(a)
+b = 0.3
+print(b)
+print(Fraction(a))
+print(Fraction(b))
+print()
+print(format(b, '0.5f'))
+print(format(b, '0.15f'))
+print(format(b, '0.25f'))
+print(Fraction(b))
+print()
 
+x = Fraction(0.3)
+print(x.limit_denominator(10))
+print()
 
+x = Fraction(math.pi)
+print(x)
+print(float(x))
+print('-'*80)
 
+print(x.limit_denominator(10))
+print(Fraction(22, 7))
+print(x.limit_denominator(100))
+print(x.limit_denominator(10000))
+print(x.limit_denominator(100000))
+print(312689/99532)
 
