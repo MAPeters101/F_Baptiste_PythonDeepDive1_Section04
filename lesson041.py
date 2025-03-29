9dbc21fe53d8d4d3fb637b112476c691b1b5caf3
Mark Peters <mapeters101@earthlink.net>
2025-03-28 23:24:51 -0400

    Work on lesson041 to 1:40.

---
+++ lesson041.py
@@ -0,0 +1,14 @@
+from math import trunc
+
+help(trunc)
+
+print(trunc(10.3))
+print(trunc(10.5))
+print(trunc(10.9))
+
+help(int)
+print(int(10.4))
+print(int(10.5))
+print(int(10.9))
+print('-'*80)
+
