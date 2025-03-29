11b5c201da0eb3a85b9758acafd22b6c0739d092
Mark Peters <mapeters101@earthlink.net>
2025-03-29 16:28:56 -0400

    Work on lesson043 from 1:50 to 6:10.

--- lesson043.py
+++ lesson043.py
@@ -7,4 +7,20 @@ a = round(1.9, 0)
 print(a, type(a))
 print('-'*80)
 
+print(round(1.8888, 3))
+print(round(1.8888, 2))
+print(round(1.8888, 1))
+print(round(1.8888, 0))
+print(0)
+
+print(round(888.88, 1))
+print(round(888.88, 0))
+print(round(888.88, -1))
+print(round(888.88, -2))
+print(round(888.88, -3))
+print(round(888.88, -4))
+print(round(9888.88, -4))
+print('-'*80)
+
+
 
