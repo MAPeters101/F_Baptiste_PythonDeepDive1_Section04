186e8b5597b1c2eeae7c6911abeb28a8aa1c33e2
Mark Peters <mapeters101@earthlink.net>
2025-03-29 16:51:09 -0400

    Work on lesson045 from 2:10 to 4:00.

--- lesson045.py
+++ lesson045.py
@@ -10,4 +10,24 @@ print(decimal.getcontext())
 print(decimal.getcontext().prec)
 print('-'*80)
 
+g_ctx = decimal.getcontext()
+print(type(g_ctx))
+print(g_ctx)
+print()
+
+g_ctx.rounding = 'ROUND_HALF_UP'
+print(g_ctx)
+print(g_ctx.rounding)
+print()
+
+g_ctx.rounding = decimal.ROUND_HALF_UP
+print(type(decimal.ROUND_HALF_UP))
+print(decimal.ROUND_HALF_UP)
+
+print(g_ctx)
+print(g_ctx.rounding)
+print()
+print('-'*80)
+
+
 
