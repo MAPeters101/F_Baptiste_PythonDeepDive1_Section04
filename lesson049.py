6e0ef63e890ecdc80e12b40e0d9a7ff67a2c437f
Mark Peters <mapeters101@earthlink.net>
2025-03-30 00:53:33 -0400

    Work on lesson049 from 3:30 to 8:00.

--- lesson049.py
+++ lesson049.py
@@ -28,10 +28,20 @@ y = Decimal(3)
 print(x//y, x%y)
 print(divmod(x, y))
 print( x == 7 * (x//y) + (x%y))
+print('-'*80)
 
+help(Decimal)
 
+a = Decimal('0.1')
+print(a.ln())
+print(a.exp())
+print(a.sqrt())
 
-
+import math
+#print(math.ln(a))
+print()
+print(math.exp(a))
+print(math.sqrt(a))
 print('-'*80)
 
 
