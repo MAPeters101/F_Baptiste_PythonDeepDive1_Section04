bebb93b1f8870264989a8629d1f58c8a332b4a63
Mark Peters <mapeters101@earthlink.net>
2025-03-29 23:45:56 -0400

    Work on lesson047 from 3:40 to 5:00.

--- lesson047.py
+++ lesson047.py
@@ -16,3 +16,11 @@ print(Decimal((0, (3,1,4,1,5), -4)))
 print(Decimal((1, (3,1,4,1,5), -4)))
 print(Decimal((1, (3,1,4,1,5), -3)))
 print('-'*80)
+
+print(format(0.1, '.25f'))
+print(Decimal(0.1))
+print(Decimal('0.1'))
+print(Decimal(0.1) == Decimal('0.1'))
+print(Decimal(10) == Decimal('10'))
+print('-'*80)
+
