c8e228b0adadae8d0bd5bcf35425de7928f84768
Mark Peters <mapeters101@earthlink.net>
2025-03-30 01:14:54 -0400

    Work on lesson049 from 3:40 to 6:40.

--- lesson050.py
+++ lesson050.py
@@ -6,4 +6,29 @@ print(sys.getsizeof(a))
 print(sys.getsizeof(b))
 print('-'*80)
 
+import time
+
+def run_float(n):
+    for i in range(n):
+        a = 3.1415
+
+def run_decimal(n):
+    for i in range(n):
+        a = Decimal('3.1415')
+
+n = 10_000_000
+start = time.perf_counter()
+run_float(n)
+end = time.perf_counter()
+print('float: ', end-start)
+
+start = time.perf_counter()
+run_decimal(n)
+end = time.perf_counter()
+print('float: ', end-start)
+print('='*80)
+
+
+
+
 
