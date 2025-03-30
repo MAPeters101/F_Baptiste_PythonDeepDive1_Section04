b9e9c748442fe3ce8a55189e3181c5edf69c321a
Mark Peters <mapeters101@earthlink.net>
2025-03-29 23:42:34 -0400

    Work on lesson047 to 3:40.

---
+++ lesson047.py
@@ -0,0 +1,18 @@
+import decimal
+from decimal import Decimal
+
+help(Decimal)
+
+print(Decimal(10))
+print(type(Decimal(10)))
+print(Decimal(-10))
+print(Decimal('10.1'))
+print(Decimal('-3.1415'))
+print()
+t = (0, (3,1,4,1,5), -4)
+print(Decimal(t))
+print(Decimal((0, (3,1,4,1,5), -4)))
+#print(Decimal(0, (3,1,4,1,5), -4))
+print(Decimal((1, (3,1,4,1,5), -4)))
+print(Decimal((1, (3,1,4,1,5), -3)))
+print('-'*80)
