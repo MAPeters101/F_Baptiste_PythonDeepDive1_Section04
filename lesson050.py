b5e6942fb9284548d77d9c9b98f6803049b7547e
Mark Peters <mapeters101@earthlink.net>
2025-03-30 01:21:48 -0400

    Complete work on lesson049 from 8:15.

--- lesson050.py
+++ lesson050.py
@@ -94,6 +94,30 @@ end = time.perf_counter()
 print('float: ', end-start)
 print('-'*80)
 
+import math
+n = 5_000_000
+def run_float(n):
+    a = 3.1415
+    for i in range(n):
+        math.sqrt(a)
+
+def run_decimal(n):
+    a = Decimal('3.1415')
+    for i in range(n):
+        a.sqrt()
+
+n = 10_000_000
+start = time.perf_counter()
+run_float(n)
+end = time.perf_counter()
+print('float: ', end-start)
+
+start = time.perf_counter()
+run_decimal(n)
+end = time.perf_counter()
+print('float: ', end-start)
+print('-'*80)
+
 
 
 
