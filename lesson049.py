eb4563806fd211035be0bab714d25fbc3fed97d0
Mark Peters <mapeters101@earthlink.net>
2025-03-30 00:47:16 -0400

    Work on lesson049 from 2:10 to 3:30.

--- lesson049.py
+++ lesson049.py
@@ -9,6 +9,31 @@ print(divmod(x, y))
 print( x == 7 * (x//y) + (x%y))
 print('-'*80)
 
+x = Decimal(10)
+y = Decimal(3)
+print(x//y, x%y)
+print(divmod(x, y))
+print( x == 7 * (x//y) + (x%y))
+print()
+
+x = -10
+y = 3
+print(x//y, x%y)
+print(divmod(x, y))
+print( x == 7 * (x//y) + (x%y))
+print()
+
+x = Decimal(-10)
+y = Decimal(3)
+print(x//y, x%y)
+print(divmod(x, y))
+print( x == 7 * (x//y) + (x%y))
+
+
+
+
+print('-'*80)
+
 
 
 
