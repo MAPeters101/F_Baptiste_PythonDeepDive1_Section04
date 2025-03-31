0d5440d9ef3cabf3b7836e54d849bfca2534fd28
Mark Peters <mapeters101@earthlink.net>
2025-03-31 15:31:19 -0400

    Work on lesson055 from 5:00 to 6:50.

--- lesson055.py
+++ lesson055.py
@@ -13,6 +13,29 @@ print(bool(0))
 print((0).__bool__())
 print('-'*80)
 
+help(list)
+a = []
+print(bool(a))
+print(a.__len__())
+print(bool(a.__len__()))
+print('-'*80)
 
+print(bool(0.0))
+print(bool(0+0j))
+print()
+
+from decimal import Decimal
+from fractions import Fraction
+print(bool(Fraction(0, 1)))
+print(bool(Fraction(1, 1)))
+print(bool(Decimal('0.0')))
+print(bool(Decimal('0.1')))
+print()
+
+print(bool(10.5))
+print(bool(1j))
+print(Fraction(1,2))
+print(Decimal('10.5'))
+print('-'*80)
 
 
