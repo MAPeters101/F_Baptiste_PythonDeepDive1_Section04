546ff849d85b29d3a5878a9a9ba2cc7a2ab4d78c
Mark Peters <mapeters101@earthlink.net>
2025-03-31 17:01:58 -0400

    Work on lesson057 from 8:50 to 11:40.

--- lesson057.py
+++ lesson057.py
@@ -50,6 +50,37 @@ print(string.ascii_letters)
 print(string.digits)
 print('-'*80)
 
+name = 'Bob'
+if name[0] in string.digits:
+    print("Name cannot start with a digit.")
+
+name = '1Bob'
+if name[0] in string.digits:
+    print("Name cannot start with a digit.")
+
+# name = ''
+# if name[0] in string.digits:
+#     print("Name cannot start with a digit.")
+
+name = ''
+if len(name) > 0 and name[0] in string.digits:
+    print("Name cannot start with a digit.")
+
+name = ''
+if len(name) and name[0] in string.digits:
+    print("Name cannot start with a digit.")
+
+name = ''
+if name and name[0] in string.digits:
+    print("Name cannot start with a digit.")
+print()
+
+name = 'abc'
+print(bool(name))
+name = ''
+print(bool(name))
+print('-'*80)
+
 
 
 
