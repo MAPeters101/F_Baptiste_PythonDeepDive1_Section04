caa988a41a89b415fc1fe383ee6fd73999c384a8
Mark Peters <mapeters101@earthlink.net>
2025-03-31 14:35:07 -0400

    Work on lesson053 to 15:25.

---
+++ lesson053.py
@@ -0,0 +1,25 @@
+help(bool)
+print(issubclass(bool, int))
+print(type(False))
+print(id(False))
+print(int(False))
+print()
+
+print(type(True))
+print(id(True))
+print(int(True))
+print()
+
+print(3 < 4)
+print(type(3 < 4))
+print(id(3 < 4))
+print()
+
+print((3 < 4) == True)
+print((3 < 4) is True)
+print()
+
+print(None is False)
+print(None == False)
+print('-'*80)
+
