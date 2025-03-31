38c066f4c71d01c492fea74c206341377d048044
Mark Peters <mapeters101@earthlink.net>
2025-03-31 13:19:41 -0400

    Work o lesson052 to 2:20.

--- lesson052.py
+++ lesson052.py
@@ -1,1 +1,17 @@
-# No code for lesson052.
+
+help(complex)
+
+a = complex(1,2)
+b = 1 + 2j
+c = 1 + 2j
+print(a == b)
+print(a == c)
+print(a.real)
+print(type(a.real))
+print(a.imag)
+print(type(a.imag))
+print(a.conjugate())
+print('-'*80)
+
+
+
