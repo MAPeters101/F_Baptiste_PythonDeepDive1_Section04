5628c19b8b60b9e841b2f1ec93ef861b19ef7811
Mark Peters <mapeters101@earthlink.net>
2025-03-31 15:24:10 -0400

    Work on lesson055 to 3:30.

---
+++ lesson055.py
@@ -0,0 +1,18 @@
+print(bool(1))
+print(bool(0))
+print(bool(-1))
+print(bool(-100))
+
+help(int)
+
+print(100 != 0)
+print(bool(100))
+print((100).__bool__())
+print()
+print(bool(0))
+print((0).__bool__())
+print('-'*80)
+
+
+
+
