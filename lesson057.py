12cae99ce776de70a860401cf0705bcea3ed4d15
Mark Peters <mapeters101@earthlink.net>
2025-03-31 16:53:10 -0400

    Work on lesson057 from 2:20 to 7:10.

--- lesson057.py
+++ lesson057.py
@@ -1,6 +1,48 @@
 print(True or True and False)
 print(True or (True and False))
 print((True or True) and False)
+print('-'*80)
+
+a = 10
+b = 2
+if a/b > 2:
+    print('a is at least twice b')
+
+# a = 10
+# b = 0
+# if a/b > 2:
+#     print('a is at least twice b')
+
+a = 10
+b = 0
+if b > 0 and a/b > 2:
+    print('a is at least twice b')
+
+if b > 0:
+    if a/b > 2:
+        print('a is at least twice b')
+
+if b > 0 and a/b > 2:
+    print('a is at least twice b')
+
+if b and a/b > 2:
+    print('a is at least twice b')
+
+b = 1
+if b and a/b > 2:
+    print('a is at least twice b')
+
+b = None
+if b and a/b > 2:
+    print('a is at least twice b')
+
+# if b > 0 and a/b > 2:
+#     print('a is at least twice b')
+print('-'*80)
+
+print('-'*80)
+
+
 
 
 
