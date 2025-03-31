0d58f02023f07f145d2b6969a811791cfb0f4973
Mark Peters <mapeters101@earthlink.net>
2025-03-31 13:53:31 -0400

    Work o lesson052 from 2:20 to 3:50.

--- lesson052.py
+++ lesson052.py
@@ -13,5 +13,19 @@ print(type(a.imag))
 print(a.conjugate())
 print('-'*80)
 
+a = 1 + 2j
+b = 10 + 8j
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a ** 2)
+print(a ** b)
+#print(a//b)
+#print(a%b)
+#print(divmod(a, b))
+print('-'*80)
+
+
 
 
