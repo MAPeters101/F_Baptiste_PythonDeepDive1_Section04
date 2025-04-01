dd44d1b6ecb968ffacb05d2c52d59debcc6580c0
Mark Peters <mapeters101@earthlink.net>
2025-04-01 14:40:26 -0400

    Work on lesson059 from 2:45 to 4:50.

--- lesson059.py
+++ lesson059.py
@@ -4,3 +4,19 @@ print('' or [1,2])
 print(1 or 1/0)
 #print(0 or 1/0)
 print('-'*80)
+
+s1 = None
+s2 = ''
+s3 = 'abc'
+
+s1 = s1 or 'n/a'
+s2 = s2 or 'n/a'
+s3 = s3 or 'n/a'
+print(s1)
+print(s2)
+print(s3)
+print([] or [0])
+print(None or [0])
+print('-'*80)
+
+
